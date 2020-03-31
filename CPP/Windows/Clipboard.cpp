850540ad45d704335bd602a41faa227c673978f3
pgp <pgp@gmx.it>
2020-03-31 15:11:17 +0200

    [1604->1700] Wim{HandlerOut,In,Register}.cpp, Clipboard.cpp

--- CPP/Windows/Clipboard.cpp
+++ CPP/Windows/Clipboard.cpp
@@ -140,8 +140,7 @@ bool ClipboardSetText(HWND owner, const UString &s)
   bool res;
   res = ClipboardSetData(CF_UNICODETEXT, (const wchar_t *)s, (s.Length() + 1) * sizeof(wchar_t));
   #ifndef _UNICODE
-  AString a;
-  a = UnicodeStringToMultiByte(s, CP_ACP);
+  AString a (UnicodeStringToMultiByte(s, CP_ACP));
   res |=  ClipboardSetData(CF_TEXT, (const char *)a, (a.Length() + 1) * sizeof(char));
   a = UnicodeStringToMultiByte(s, CP_OEMCP);
   res |=  ClipboardSetData(CF_OEMTEXT, (const char *)a, (a.Length() + 1) * sizeof(char));
