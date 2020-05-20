459a508660c1590273dd44059f249d2be570669c
pgp <pgp@gmx.it>
2020-05-20 17:00:10 +0200

    [1604->1700] IsoHandler.cpp; [PARTIAL][INSERTIONS_COMPLETED] ItemNameUtils.{h,cpp}

--- CPP/7zip/Archive/Common/ItemNameUtils.h
+++ CPP/7zip/Archive/Common/ItemNameUtils.h
@@ -8,19 +8,37 @@
 namespace NArchive {
 namespace NItemName {
 
+//@@@@@ BEGIN REMOVE BLOCK
   void ReplaceToOsPathSeparator(wchar_t *s);
 
   UString MakeLegalName(const UString &name);
   UString GetOSName(const UString &name);
   UString GetOSName2(const UString &name);
   void ConvertToOSName2(UString &name);
+//@@@@@ END REMOVE BLOCK
+
+void ReplaceSlashes_OsToUnix(UString &name);
+  
+UString GetOsPath(const UString &name);
+UString GetOsPath_Remove_TailSlash(const UString &name);
+  
+void ReplaceToOsSlashes_Remove_TailSlash(UString &name);
+
   bool HasTailSlash(const AString &name, UINT codePage);
 
+//@@@@@ BEGIN REMOVE BLOCK
   #ifdef _WIN32
   inline UString WinNameToOSName(const UString &name)  { return name; }
   #else
   UString WinNameToOSName(const UString &name);
   #endif
+//@@@@@ END REMOVE BLOCK
+
+#ifdef _WIN32
+  inline UString WinPathToOsPath(const UString &name)  { return name; }
+#else
+  UString WinPathToOsPath(const UString &name);
+#endif
 
 }}
 
